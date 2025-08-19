2c09f27b663af607c52df3a931f70bb509068c77
Gerald Leenerts <gerald.leenerts@gmail.com>
2025-08-19 16:40:26 -0700

    Simple way to map sensors to named values

--- hardware/blink.py
+++ hardware/blink.py
@@ -3,6 +3,7 @@ import onewire
 import ds18x20
 import time
 import binascii
+import sys
 
 def cToF(c):
     return (c*(9/5)) + 32
@@ -10,10 +11,30 @@ def cToF(c):
 gp_pin = machine.Pin(26)
 
 ds18b20_sensor = ds18x20.DS18X20(onewire.OneWire(gp_pin))
+sensor_mapping = {
+    '280943230d0000c7': 'A Side',
+    '28f475b80e000076': 'B Side'
+}
+
+def getMissingMappedSensors(all_sensors):
+    missing_mappings = []
+    for device in all_sensors:
+        s = binascii.hexlify(device)
+        readable_string = s.decode('ascii')
+        if not (readable_string in sensor_mapping) :
+            missing_mappings.append(readable_string)
+    return missing_mappings
 
 sensors = ds18b20_sensor.scan()
 
-print('Found devices: ', sensors)
+missing_mappings = getMissingMappedSensors(sensors)
+is_missing_mappings = len(missing_mappings) != 0;
+
+if(is_missing_mappings) :
+    print('Missing Mappings, please identify...')
+    print(list(missing_mappings))
+
+print ('All Sensors Found and Mapped. Starting')
 
 while True:
     ds18b20_sensor.convert_temp()
@@ -21,11 +42,14 @@ while True:
     for device in sensors:
         s = binascii.hexlify(device)
         readable_string = s.decode('ascii')
-        print(readable_string)
+        
+        name = readable_string
+        if (readable_string in sensor_mapping) :
+            name = sensor_mapping[readable_string]
+        
         c_raw = ds18b20_sensor.read_temp(device)
         f_raw = cToF(c_raw)
         c = round(c_raw,1)
         f = round(f_raw,1)
-        print(f'{c} C, {f} F')
+        print(f'{name}: {c} C, {f} F')
     time.sleep(1)
-
