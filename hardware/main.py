53de6480f86da566732e46b94c3adbe4ab4c7f38
Gerald Leenerts <gerald.leenerts@gmail.com>
2025-08-19 18:37:50 -0700

    Write to a file and rename so program runs on startup

---
+++ hardware/main.py
@@ -0,0 +1,107 @@
+from machine import Pin
+import onewire
+import ds18x20
+import time
+import binascii
+
+async def setTime(loop=0):
+    #ntptime.settime() failure: [Errno 110] ETIMEDOUT
+    #ntptime.settime() failure: overflow converting long int to machine word
+    print('Attempt to set time:',1+loop)
+    try:
+        if loop<5:
+            d=time.time()-debug.poweron.time
+            ntptime.settime()
+            debug.poweron.time=time.time()-d
+            logging.clock.method="accurate"
+        else:
+            # if it did not work the second time it is not going to in my testing
+            print("Failed to set time; Waiting 15 seconds")
+            await sleep(15000)
+            if logging.clock.time_delta:
+                # If I do not have this by now something is very wrong
+                # WiFi is working, therefore my server is up and has configured settings
+                print("Setting clock using low precision method")
+                from machine import RTC
+                d=time.time()-debug.poweron.time
+                t=time.gmtime(time.time()+logging.clock.time_delta)
+                RTC().datetime((t[0], t[1], t[2], t[6], t[3], t[4], t[5], 0))
+                debug.poweron.time=time.time()-d
+                print("Result:",debugData(time.time()))
+                logging.clock.method="low precision"
+            else:
+                # So lets get this strait:
+                # The server's Guest OS (pfsense) has asigned a IP to us over WiFi
+                # The server host is unrechable
+                # How is that even possible
+                print("Rebooting in 15 seconds, failed to set clock")
+                await sleep(15000)
+                from machine import reset
+                reset()
+        logging.clock.sync=time.time()
+    except OverflowError:
+        # it is not going to work
+        print("overflow error; settime is borked")
+        await setTime(9001)
+    except OSError:
+        print("ntptime.settime() failure")
+        if loop>0:
+            # Not expecting more than 1 failure, maybe waiting will help?
+            await sleep(10000)
+        await setTime(loop+1)
+
+setTime()
+
+test_file = open("test.csv","w")
+
+def cToF(c):
+    return (c*(9/5)) + 32
+
+gp_pin = Pin(26)
+
+ds18b20_sensor = ds18x20.DS18X20(onewire.OneWire(gp_pin))
+sensor_mapping = {
+    '280943230d0000c7': 'A Side',
+    '28f475b80e000076': 'B Side'
+}
+
+def getSensorId(device):
+    s = binascii.hexlify(device)
+    return s.decode('ascii')
+
+def getSensorName(device):
+    id = getSensorId(device)
+    if (id in sensor_mapping) :
+        return sensor_mapping[id]
+    else :
+        return id
+
+sensors = ds18b20_sensor.scan()
+
+number_devices = len(sensors)
+
+names = list(map(getSensorName, sensors))
+test_file.write('Time,'+','.join(names)+'\n')
+test_file.flush()
+
+print('Number of sensors: ', number_devices)
+
+while True:
+    ds18b20_sensor.convert_temp()
+    time.sleep_ms(750)
+    newLine = [str(time.localtime())]
+    for device in sensors:
+        name = getSensorName(device)
+        c_raw = ds18b20_sensor.read_temp(device)
+        newLine.append(str(c_raw))
+        f_raw = cToF(c_raw)
+        c = round(c_raw,1)
+        f = round(f_raw,1)
+        print(f'{name}: {c} C, {f} F')
+
+    test_file.write(','.join(newLine)+'\n')
+    test_file.flush()
+    time.sleep(60)
+
+
+
